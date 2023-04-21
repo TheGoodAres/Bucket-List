42ff15e11d40a5b29554437399e189b173c9f6d5
TheGoodAres <61661335+TheGoodAres@users.noreply.github.com>
2023-04-21 09:19:25 +0100

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Robert-Dumitru Oprea on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
